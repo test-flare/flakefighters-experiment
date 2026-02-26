64b644e0c6d57da6381f8fde796cd9b205ab1853
Michael Foster <m.foster@sheffield.ac.uk>
2026-02-26 10:32:33 +0000

    Cleaned up flakiness reproduction

--- src/reproduce_flakiness.py
+++ src/reproduce_flakiness.py
@@ -14,7 +14,7 @@ REPO_PATH = "./core"  # Path where the repo will be cloned
 REPO_URL = "https://github.com/home-assistant/core.git"
 ITERATIONS = 2
 
-replacements = {"mypy-dev==1.12.0a2": "mypy-dev", "aioasuswrt==1.5.1": "aioasuswrt==1.5.2"}
+replacements = {r"mypy\-dev==\d+\.\d+\.\w+": "mypy-dev", r"aioasuswrt==1\.5\.1": "aioasuswrt==1.5.2"}
 
 
 def parse_test_failures(content):
@@ -35,7 +35,7 @@ def search_for_flakiness(test_path, db_url):
         with open(fname) as f:
             packages = f.readlines()
         for old, new in replacements.items():
-            packages = [p.replace(old, new) for p in packages]
+            packages = [re.sub(old, new, p) for p in packages]
         with open(fname, "w") as f:
             f.write("\n".join(packages))
 
@@ -91,7 +91,20 @@ def search_for_flakiness(test_path, db_url):
             test_to_run = test_path
         else:
             failures += 1
+
+    assert os.path.exists(db_url), f"No database at {db_url}"
     return {"successes": successes, "failures": failures, "confirmed": confirmed}
+
+
+def check_sha(repo, target_sha, test_path, db_url, source_sha=None):
+    repo.git.checkout(target_sha)
+    if source_sha is not None:
+        repo.remotes.origin.fetch(source_sha)
+        repo.git.merge("FETCH_HEAD")
+
+    flakiness = search_for_flakiness(test_path, db_url)
+    repo.git.reset("--hard")
+    return flakiness
 
 
 def main():
@@ -101,30 +114,21 @@ def main():
     )
     parser.add_argument("-s", "--source-sha", help="Source commit sha.")
     parser.add_argument("-t", "--target-sha", help="Target commit sha.")
-    parser.add_argument("-S", "--sample-shas", nargs="+", help="List of historic commits to run the tests on.")
     parser.add_argument("-T", "--test-path", help="Name of the test to run.")
+    parser.add_argument("-S", "--sample-shas", nargs="+", help="List of historic commits to run the tests on.")
     parser.add_argument("-o", "--output", help="Output file path.")
     args = parser.parse_args()
 
+    db_url = f"sqlite:///{args.output.replace('.json', '.db')}"
+
     repo = Repo(REPO_PATH)
-    repo.git.checkout("--", "pyproject.toml")
-    repo.git.checkout("--", "*.txt")
+    repo.git.reset("--hard")
     repo.git.fetch()
 
     flakiness = {}
     for sha in args.sample_shas:
-        repo.git.checkout(sha)
-        flakiness[sha] = search_for_flakiness(args.test_path, f"sqlite:///{args.output.replace('.json', '.db')}")
-        repo.git.checkout("--", "pyproject.toml")
-        repo.git.checkout("--", "*.txt")
-
-    repo.git.checkout(args.target_sha)
-    repo.remotes.origin.fetch(args.source_sha)
-    repo.git.merge("FETCH_HEAD")
-
-    flakiness["PR"] = search_for_flakiness(args.test_path, f"sqlite:///{args.output.replace('.json', '.db')}")
-    repo.git.checkout("--", "pyproject.toml")
-    repo.git.checkout("--", "*.txt")
+        flakiness[sha] = check_sha(repo, sha, args.test_path, db_url)
+    flakiness[args.target_sha] = check_sha(repo, args.target_sha, args.test_path, db_url, args.source_sha)
 
     with open(args.output, "w") as f:
         json.dump(
