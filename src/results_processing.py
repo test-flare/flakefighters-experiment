846b6653679be6ccfd6f4499bd267f70b2a38960
Michael Foster <m.foster@sheffield.ac.uk>
2026-04-30 16:09:29 +0100

    Better results processing

--- src/results_processing.py
+++ src/results_processing.py
@@ -13,40 +13,38 @@ from pathlib import Path
 test_name_re = re.compile(r"(tests/[\w\/\.\d_]+::[\w\d_]+)")
 
 
+def get_flakefighter_data(file: str):
+    runs = Database(f"sqlite:///{os.path.abspath(file)}").load_runs()
+    for run in runs:
+        for test in run.tests:
+            datum = {f.name: f.flaky for f in test.flakefighter_results}
+            for execution in test.executions:
+                # Only want to log the first failing execution as the "result"
+                if execution.outcome == "failed":
+                    datum |= {f.name: f.flaky for f in execution.flakefighter_results}
+                    return datum
+    return {}
+
+
 def main():
     data = []
-    confirmed = 0
-    total = 0
-    only_passed = 0
-    only_failed = 0
-    for file in sorted(glob("outputs/tests/components/**/*.db", recursive=True)):
+
+    results_files = sorted(glob("outputs/tests/components/**/*.db", recursive=True))
+    for file in results_files:
         with open(file.replace(".db", ".json")) as f:
             log = json.load(f)
-        confirmed += log["flakiness"]["confirmed"]
-        total += 1
-        runs = Database(f"sqlite:///{os.path.abspath(file)}").load_runs()
-        only_passed += all(e.outcome == "passed" for run in runs for test in run.tests for e in test.executions)
-        only_failed += all(e.outcome == "failed" for run in runs for test in run.tests for e in test.executions)
-        for run in runs:
-            for test in run.tests:
-                datum = {"test": test.name, "commit": Path(file).stem}
-                for f in test.flakefighter_results:
-                    datum[f.name] = f.flaky
-                if test_name_re.search(file).group(1) == test_name_re.search(test.name).group(1):
-                    for execution in test.executions:
-                        x_datum = (
-                            dict(datum)
-                            | {"outcome": execution.outcome, "reproduced": log["flakiness"]["confirmed"]}
-                            | {f.name: f.flaky for f in execution.flakefighter_results}
-                        )
-                        data.append(x_datum)
+        datum = {
+            "test": Path(file).parts[-2],
+            "source_sha": log["source_sha"],
+            "target_sha": log["target_sha"],
+            "confirmed": log["flakiness"]["confirmed"],
+            "successes": log["flakiness"]["successes"],
+            "failures": log["flakiness"]["failures"],
+        } | get_flakefighter_data(file)
+        data.append(datum)
 
     data = pd.DataFrame(data)
     data.to_csv("results.csv")
-    print(data)
-    print(f"confirmed {confirmed}/{total}")
-    print(f"{only_passed} only passed")
-    print(f"{only_failed} only failed")
 
 
 if __name__ == "__main__":
